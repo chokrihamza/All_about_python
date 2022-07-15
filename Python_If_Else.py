e1b933ee77046b987fbede1bd84b85c5bbbbaf2b
chokrihamza <chokrihamza1994@gmail.com>
2022-07-15 16:42:39 +0200

    pytohn if else

---
+++ Python_If_Else.py
@@ -0,0 +1,30 @@
+a = 33
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
+
+#short hand if
+
+if a >b:print("a is greater than b")
+
+#short hand if else
+a=30
+b=300
+print("A")if a>b else print("B")
+#One line if else statement, with 3 conditions:
+a=60
+b=60
+print("A") if a>b else print("=") if a==b else print("B")
+
+# in the in condition you can use or and and nested if
